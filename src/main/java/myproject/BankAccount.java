33febade9fdd9be40048256564434439ea4fc23b
Diaboy01 <marvin.prigenitz@gmx.de>
2022-04-11 15:06:02 +0000

    Fixed

--- src/main/java/myproject/BankAccount.java
+++ src/main/java/myproject/BankAccount.java
@@ -1,20 +1,22 @@
+package myproject;
+
 public class BankAccount {
 	
 	long balance;
 
-	private long getBlance() {
+	public long getBalance() {
 		return this.balance;
 	}
 
-	private void deposit(long value) {
+	public void deposit(long value) {
 		this.balance = this.balance + value;
 	}
 
-	private void withdraw(long value) {
+	public void withdraw(long value) {
 		this.balance = this.balance - value;
 	}
 
-	private void withdraw(long foreignValue, double conversionfactor) {
+	public void withdraw(long foreignValue, double conversionfactor) {
 		long factor = Math.round(conversionfactor);
 		long value = foreignValue*factor;
 		this.balance = this.balance - value;
--- src/main/java/myproject/Greeter.java
+++ src/main/java/myproject/Greeter.java
@@ -1,9 +1,12 @@
 public class Greeter {
 String name;
+
 public Greeter(String name) {
 this.name = name;
 }
 public String greet() {
-return "Hello " + name + ", nice to meet you!");
+return "Hello " + name + ", nice to meet you!";
 }
 }
+
+
